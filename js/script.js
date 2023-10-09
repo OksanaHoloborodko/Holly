72426c62a5920614a2fc398aa044cbcbe1ba3c0d
OksanaHoloborodko <holoborodko.oksana@gmail.com>
2023-10-09 11:39:27 +0300

    feat: add startDate and endate Listeners

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 const tab = document.querySelector('.tab');
 const tabDate = document.querySelector('.tab-date');
 const tabHoliday = document.querySelector('.tab-holiday');
+const startDate = document.getElementById('startDate');
+const endDate = document.getElementById('endDate');
 
 const changeTab = (event) => {
     const tabButtons = document.querySelectorAll('.tab__button');
@@ -24,4 +26,30 @@ const changeTab = (event) => {
     }
 };
 
+function getNormalizeDate(date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
+    const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
+
+    return `${year}-${month}-${day}`;
+}
+
+const startDateListener = () => {
+    const startDateValue = new Date(startDate.value);
+    const endDateValue = new Date(endDate.value);
+
+    endDate.setAttribute('min', getNormalizeDate(startDateValue));
+
+    endDate.removeAttribute('disabled');
+};
+
+const endtDateListener = () => {
+    const startDateValue = new Date(startDate.value);
+    const endDateValue = new Date(endDate.value);
+
+    startDate.setAttribute('max', getNormalizeDate(endDateValue));
+};
+
 tab.addEventListener('click', changeTab);
+startDate.addEventListener('input', startDateListener);
+endDate.addEventListener('input', endtDateListener);
