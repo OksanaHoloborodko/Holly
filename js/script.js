596ddf21de788ed296cc780da8b1658d63dbb6e6
OksanaHoloborodko <holoborodko.oksana@gmail.com>
2023-10-10 18:09:17 +0300

    feat: add durationBetweenDates function

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const tabHoliday = document.querySelector('.tab-holiday');
 const startDate = document.getElementById('startDate');
 const endDate = document.getElementById('endDate');
 const preset = document.querySelector('.preset');
+const countBtn = document.querySelector('.count__button');
+const resultText = document.querySelector('.result__text');
 
 const changeTab = (event) => {
     const tabButtons = document.querySelectorAll('.tab__button');
@@ -105,8 +107,69 @@ function countWeekdays(startDate, endDate) {
     return count;
 };
 
+function normalizeDimension(result, dimension) {
+    return result == 1 ? dimension.slice(0, -1).toLowerCase() : dimension.toLowerCase();
+}
+
+const durationBetweenDates = () => {
+    const startDateValue = new Date(startDate.value);
+    const endDateValue = new Date(endDate.value);
+    const dayOption = document.getElementById('selectDays').value;
+    const dimension = document.getElementById('selectUnits').value;
+    let durationDays;
+
+    const DAY_IN_MILLISECONDS = 86400000;
+    const HOURS_IN_DAY = 24;
+    const MINUTES_IN_DAY = 1440;
+    const SECONDS_IN_DAY = 86400;
+
+    if(dayOption === 'All days') {
+        durationDays = (endDateValue - startDateValue) / DAY_IN_MILLISECONDS;
+    } else if (dayOption === 'Weekdays') {
+        durationDays = countWeekdays(startDateValue, endDateValue);
+    } else if (dayOption === 'Weekends') {
+        durationDays = countWeekends(startDateValue, endDateValue);
+    }
+
+    let result;
+
+    switch (dimension) {
+        case 'Days':
+            result = durationDays;
+        break;
+        case 'Hours':
+            result = durationDays * HOURS_IN_DAY;
+        break;
+        case 'Minutes':
+            result = durationDays * MINUTES_IN_DAY;
+        break;
+        case 'Seconds':
+            result = durationDays * SECONDS_IN_DAY;
+        break;
+     }
+
+     if(isNaN(result)) {
+        return;
+     }
+
+     let paragraph = resultText.querySelector("p");
+
+     if(!paragraph) {
+        paragraph = document.createElement('p');
+        paragraph.textContent = `${result} ${normalizeDimension(result, dimension)}`;
+        resultText.appendChild(paragraph);
+
+        const resultBlock = document.querySelector('.result-date');
+        resultBlock.classList.remove('result');
+     } else {
+        paragraph.textContent = `${result} ${normalizeDimension(result, dimension)}`;
+     }
+}
+
 
 tab.addEventListener('click', changeTab);
 startDate.addEventListener('input', startDateListener);
 endDate.addEventListener('input', endtDateListener);
 preset.addEventListener('click', choosePreset);
+countBtn.addEventListener('click', durationBetweenDates);
+
